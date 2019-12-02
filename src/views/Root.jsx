46955cc2483016fa3538056885efdf9b59557147
christianahvilla <christianahvilla@gmail.com>
2019-12-02 17:02:16 -0600

    Add initial configuration

---
+++ src/views/Root.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Route, Switch, withRouter} from 'react-router-dom';
+
+const Root = () => {
+  <Switch>
+    <Route/>
+  </Switch>;
+};
+
+export default withRouter(Root);
