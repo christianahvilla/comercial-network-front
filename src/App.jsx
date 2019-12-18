b06876a6286f4d77c8c107f797a32b50f9d392e1
christianahvilla <christianahvilla@gmail.com>
2019-12-18 15:55:11 -0600

    Remove dependicies

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import React from 'react';
-import {Provider} from 'react-redux';
-import PropTypes from 'prop-types';
-import {PersistGate} from 'redux-persist/integration/react';
-import LoadingView from './components/common/LoadingView';
-import Root from './views/Root';
 
-App.propTypes = {
-  store: PropTypes.object.isRequired,
-};
 
-const App = ({store}) => (
-  <Provider store={store}>
-    <PersistGate loading={<LoadingView/>} persistor={persistor}>
-      <Root/>
-    </PersistGate>
-  </Provider>
+const App = () => (
+  <div>fsfsaf</div>
 );
 
 export default App;
