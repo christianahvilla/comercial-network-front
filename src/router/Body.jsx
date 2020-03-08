d432aa33f36bec5dbce648d136130feaed07b0d4
Daniel <danielgswtu@hotmail.com>
2020-03-08 00:36:23 -0600

    Users CRUD

--- src/router/Body.jsx
+++ src/router/Body.jsx
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import Home from '../views/Home';
 import Category from '../views/Category';
 import NotFound from '../views/NotFound';
+import User from '../views/User';
 
 const Body = (props) => {
     const { path } = props;
@@ -10,7 +11,7 @@ const Body = (props) => {
         <Switch>
             <Route exact path={`${path}/categories`} component={Category} />
             <Route exact path={`${path}/shops`} component={NotFound} />
-            <Route exact path={`${path}/users`} component={NotFound} />
+            <Route exact path={`${path}/users`} component={User} />
             <Route exact path={`${path}`} component={Home} />
             <Route path="*" component={NotFound} />
         </Switch>
